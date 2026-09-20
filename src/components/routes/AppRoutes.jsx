a3eaa9289446d9254b97065678697b9b32e6e85b
mursalumeyma-alt <YOUR_GITHUB_EMAIL>
2026-09-20 14:38:44 +0545

    Restore Ziyaarah page routes

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -4,7 +4,14 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import DashboardLayout from "../layouts/DashboardLayout";
-
+import AboutUs from "../pages/AboutUs";
+import Prayers from "../pages/Prayers";
+import Events from "../pages/Events";
+import Contact from "../pages/Contact";
+import Trips from "../pages/Trips";
+import Resources from "../pages/Resources";
+import RitualTracker from "../pages/RitualTracker";
+import TripPlanner from "../pages/TripPlanner";
 export default function AppRoutes() {
   return (
     <Routes>
@@ -12,7 +19,16 @@ export default function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<DashboardLayout />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-       </Routes>
+<Route path="/about" element={<AboutUs />} />
+    <Route path="/forgot-password" element={<ForgotPassword />} />
+    <Route path="/prayers" element={<Prayers />} />
+    <Route path="/events" element={<Events />} />
+    
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/trips" element={<Trips />} />
+         <Route path="/resources" element={<Resources />} />
+         <Route path="/ritual-tracker" element={<RitualTracker />} />
+         <Route path="/trip-planner" element={<TripPlanner />} />
+    </Routes>
   );
 }
